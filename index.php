66141883880af20abe048ceb3576176e54dab4c5
Dennis Wallace <denniswallace10@gmail.com>
2016-11-14 14:30:56 +0100

    Login ins index.php

--- index.php
+++ index.php
@@ -1,1 +1,123 @@
-hello
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>p42 - Login</title>
+
+    <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="css/login.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <!-- http://bootsnipp.com/snippets/featured/login-and-register-tabbed-form -->
+    <div class="container">
+    	<div class="row">
+  			<div class="col-md-6 col-md-offset-3">
+  				<div class="panel panel-login">
+  					<div class="panel-heading">
+  						<div class="row">
+  							<div class="col-xs-12">
+    							<h3> Wissenstransfer Jahresbericht</h3>
+  							</div>
+  						</div>
+
+  						<div class="row">
+  							<div class="col-xs-6">
+  								<a href="#" class="active" id="login-form-link">Login</a>
+  							</div>
+  							<div class="col-xs-6">
+  								<a href="#" id="register-form-link">Anmelden</a>
+  							</div>
+  						</div>
+  						<hr>
+  					</div>
+  					<div class="panel-body">
+  						<div class="row">
+  							<div class="col-lg-12">
+  								<!-- Login-Formular -->
+  								<form id="login-form" action="index.php" method="post" role="form" style="display: block;">
+  									<div class="form-group">
+  										<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="E-Mail-Adresse" value="">
+  									</div>
+  									<div class="form-group">
+  										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Passwort">
+  									</div>
+  									<div class="form-group">
+  										<div class="row">
+  											<div class="col-sm-6 col-sm-offset-3">
+  												<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="einloggen">
+  											</div>
+  										</div>
+  									</div>
+  								</form>
+  								<!-- /Login-Formular -->
+
+  								<form id="register-form" action="index.php" method="post" role="form" style="display: none;">
+  									<div class="form-group">
+  										<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="E-Mail-Adresse" value="">
+  									</div>
+  									<div class="form-group">
+  										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Passwort">
+  									</div>
+  									<div class="form-group">
+  										<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Passwort bestätigen">
+  									</div>
+  									<div class="form-group">
+  										<div class="row">
+  											<div class="col-sm-6 col-sm-offset-3">
+  												<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="jetzt registrieren">
+  											</div>
+  										</div>
+  									</div>
+  								</form>
+
+  							</div>
+  						</div>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+  		<!-- Die beiden BS-Allert-Boxen geben Rückmeldung über den Registrierungsprozess. Sie werden erst später benötigt. -->
+      <!--
+      <div class="alert alert-success" role="alert">Es hat funktioniert.</div>
+      <div class="alert alert-danger" role="alert">Es ist ein Fehler aufgetreten.</div>
+      -->
+    </div><!-- /container -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script>
+      $(function() {
+
+        $('#login-form-link').click(function(e) {
+      		$("#login-form").delay(100).fadeIn(100);
+       		$("#register-form").fadeOut(100);
+      		$('#register-form-link').removeClass('active');
+      		$(this).addClass('active');
+      		e.preventDefault();
+      	});
+
+      	$('#register-form-link').click(function(e) {
+      		$("#register-form").delay(100).fadeIn(100);
+       		$("#login-form").fadeOut(100);
+      		$('#login-form-link').removeClass('active');
+      		$(this).addClass('active');
+      		e.preventDefault();
+      	});
+
+      });
+    </script>
+
+  </body>
+</html>
