0c7e55c308aaa1d15a89a483e43fd22f904fa2f8
Akin Salur +41(0)787051453 <salur.akin@gmail.com>
2016-11-15 12:01:16 +0100

    Optionen definiert

---
+++ optionen.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Optionen</title>
+  <meta charset="utf-8">
+  <link href="optionen.css" rel="stylesheet" type="text/css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="buttons">
+<div class="content">
+  <a href="home_forschungsleiter.php" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-folder-open"></span> Publikationen</a>
+
+  <a href="home_dozent.php" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-edit"></span> Publikation erfassen</button>
+
+</div>
+</div>
+
+</body>
+</html>
