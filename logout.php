576430baa4b0e1307c49a083724a9aa99d746824
Rosa Künzler <rosa.kuenzler@gmx.ch>
2016-11-24 11:29:21 +0100

    location auf optionen.css abgändert

--- logout.php
+++ logout.php
@@ -1,5 +1,24 @@
 
-        <form action="index.php" method="post">
-            <input name="return" type="hidden" value="<?php echo urlencode($_SERVER["PHP_SELF"]);?>" />
-            <input type="submit" value="logout" />
-        </form>
+          <form action="index.php" method="post" class="button">
+              <input name="return" type="hidden" align="right" value="<?php echo urlencode($_SERVER["PHP_SELF"]);?>" />
+              <input type="submit" value="Logout" />
+          </form>
+
+
+          .button{
+              float:right;
+              border: none;
+              color: black;
+              padding: 15px 32px;
+              text-decoration: none;
+              display: inline-block;
+              font-size: 16px
+          }
+
+
+          <!-- Logout Button -->
+
+            <form action="index.php" method="post" class="button">
+                  <input name="return" type="hidden" align="right" value="<?php echo urlencode($_SERVER["PHP_SELF"]);?>" />
+                  <input type="submit" value="Logout" />
+            </form>
