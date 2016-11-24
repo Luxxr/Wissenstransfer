536fc2a049cc4f112e4379cc56df29d64ac57f1b
Akin Salur +41(0)787051453 <salur.akin@gmail.com>
2016-11-24 10:14:03 +0100

    edit

--- optionen.php
+++ optionen.php
@@ -3,17 +3,24 @@
 <head>
   <title>Optionen</title>
   <meta charset="utf-8">
-  <link href="optionen.css" rel="stylesheet" type="text/css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href="optionen.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+  <div class="uberschrift">
+    <h1>Startseite</h1>
+  </div>
+<div class="umrandung"></div>
+<div class="umrandung">
 <div class="buttons">
 <div class="content">
   <a href="home_forschungsleiter.php" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-folder-open"></span> Publikationen</a>
-  <a href="home_dozent.php" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-edit"></span> Publikation erfassen</button>
+  <a href="home_dozent.php" class="btn btn-info btn-lg" ><span class="glyphicon glyphicon-edit"></span> Publikation erfassen</button>
+  <a href="#" class="btn btn-lg btn-danger" disabled="disabled"><span class="glyphicon glyphicon-lock"></span> Privat</a>
+</div>
 </div>
 </div>
 </body>
